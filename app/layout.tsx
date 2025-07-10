aea31c12179e8550b819f312c967e769d61b45dd
Saron <saronawoke635@gmail.com>
2025-07-10 18:41:59 +0300

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,42 @@
-import type { Metadata } from 'next'
-import './globals.css'
+ 
+
+import type { Metadata } from "next";
+import "./globals.css";
+
+import {
+  Cinzel_Decorative,
+  Josefin_Sans,
+  Lora,
+  Noto_Kufi_Arabic,
+} from "next/font/google";
+
+const josefinSans = Josefin_Sans({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700"],
+  style: ["normal", "italic"],
+  variable: "--font-josefin-sans",
+  display: "swap",
+});
+
+
 
 export const metadata: Metadata = {
-  title: 'Graduation Book',
-  description: 'Digital Graduation Book',
-  icons: {
-    icon: '/favicon.ico',
-  },
-}
+  title: "Graduation Book",
+  description: "Digital Graduation Book",
+  generator: "Next.js",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className="font-[poppins]">
-        {children}
-      </body>
+    <html
+      lang="en"
+      className={josefinSans.className}
+    >
+      <body className={josefinSans.className}>{children}</body>
     </html>
-  )
+  );
 }
